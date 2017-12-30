24024d3ea41e8fccb243e07e476d7fe06e6fb9d5
Michal Sakowicz <sakowiczm@gmail.com>
2017-12-30 20:39:21 +0000

    dfas

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -21,11 +21,11 @@ namespace tests
         public void SortShouldOrderResults()
         {
             var range = new Range { Count = 3, Sort = true };
-            var values = new[] { "a", "b", "c" };
+            var values = new[] { "a", "c", "b" };
             var counter = 0;
             var generated = range.Of(() => values[counter++]);
 
-            Assert.Equal(new[] { "a", "b". "c" }, generated.ToArray());
+            Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
         }
     }
 }
